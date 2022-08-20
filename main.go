b161ce1d69f3502a379c65475f2e1a90c9079e39
Vsevolod <sevakoltypin@gmail.com>
2022-08-20 22:29:11 +0300

    start of project

---
+++ main.go
@@ -0,0 +1,19 @@
+package kolgram
+
+import (
+	"log";
+	"github.com/Syfaro/telegram-bot-api";
+	//"kolgram";
+	//"SQL";
+	//"yamlReader";
+	//"logic"
+)
+
+func main() {
+	//parametrs := yamlReader.GetYamlFile('parameters.yaml')
+
+	//connection := SQL.GetConnection()
+
+	//EvaGoer := kolgram.GetConnection(parametrs['token'])
+
+}
