c90d5515d06c31d487c65bc87efd2b7c66f2e09d
koltypka <sevakoltypin@gmail.com>
2023-03-08 00:50:42 +0300

    правки, рефакторинг

--- Service/request.go
+++ Service/request.go
@@ -1,12 +1,47 @@
 package request
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/koltypka/kolgram/myErr"
+)
 
 type Request struct {
-	url    string
+	host   string
 	client http.Client
 }
 
-func New(url string) Request {
-	return Request{url: url, client: http.Client{}}
+func New(host string) Request {
+	return Request{host: host, client: http.Client{}}
+}
+
+func (r *Request) run(method string, query url.Values) (data []byte, err error) {
+	defer func() {err = myErr.Handle("Request error", err)}
+
+	myUrl := url.URL{Scheme: "https", Host: r.host, Path: method}
+
+	req, err := http.NewRequest(http.MethodGet, myUrl.String(), nil)
+
+	if err != nil {
+		return nil,err 
+	}
+	
+	req.URL.RawQuery = query.Encode()
+
+	result, err := r.client.Do(req)
+
+	if err != nil {
+		return nil, err 
+	}
+
+	defer func(){_=result.Body.Close()}()
+
+	body, err := io.ReadAll(result.Body)
+	if err != nil {
+		return nil, err 
+	}
+
+	return body, nil
 }
